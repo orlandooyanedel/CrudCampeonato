afa250c6aa74e16227ffeb117b80f0f96d48cd27
cetecom <cetecom@LCV11024>
2019-05-07 15:06:15 -0300

    Primera subida

---
+++ lib/Constantes.php
@@ -0,0 +1,17 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+session_start();
+define("URLBASE",'http://localhost:8081/CrudCampeonato/');
+define("DIRBASE",'C:/xampp/htdocs/CrudCampeonato');
+/*
+ * Parametros DB
+ * **/
+define("DBNAME",'equiposfutbol');
+define("DBUSR",'duoc');
+define("DBPWD",'');
+define("DBHOST",'localhost');
